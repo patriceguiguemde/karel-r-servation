bd926e4afff7b00af232f2c38ddb1665e55a7348
Guiguemde-lang <patriceguiguemde1@gmail.com>
2026-05-28 20:49:02 +0100

    commentaire de dans la page admin

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -94,7 +94,8 @@ export default function AdminDashboard() {
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
           <h1 className="text-2xl font-bold text-gray-800">Administrateur</h1>
           <div className="flex items-center gap-4">
-            <span className="text-sm text-gray-600"> {user?.name} ({user?.role})</span>
+            
+            {/*<span className="text-sm text-gray-600"> {user?.name} ({user?.role})</span>*/}
             <button onClick={logout} className="px-4 py-2 text-sm text-red-600 hover:bg-red-50 rounded-lg">
               Déconnexion
             </button>
